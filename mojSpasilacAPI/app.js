140003577c634638d8b809d6b9c6db7488f152f1
mihailot01 <mihailo.t.001@gmail.com>
2021-04-10 03:25:49 +0200

    base url

--- mojSpasilacAPI/app.js
+++ mojSpasilacAPI/app.js
@@ -20,9 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/korisnici', require('./routes/korisnici'));
-app.use('/prijave', require('./routes/prijave'));
+app.use(process.env.BASE_URL, indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- mojSpasilacAPI/routes/index.js
+++ mojSpasilacAPI/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.use('/korisnici', require('./../routes/korisnici'));
+router.use('/prijave', require('./../routes/prijave'));
+
 module.exports = router;
